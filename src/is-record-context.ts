87a69ab85bff4cc6567dc205fec1d8f0b521b7da
Eric Crosson <eric.s.crosson@utexas.edu>
2019-03-05 11:14:56 -0600

    Initial commit

---
+++ src/is-record-context.ts
@@ -0,0 +1,12 @@
+/**
+ * is-record-context
+ * Test to determine if an object implements RecordContext
+ */
+
+
+/**
+ * TODO: document
+ */
+export default function isRecordContext() {
+    // TODO: implement
+}
---
+++ test/test-is-record-context.ts
@@ -0,0 +1,13 @@
+import test from 'ava'
+
+/**
+ * Library under test
+ */
+
+import isRecordContext from '../src/is-record-context'
+
+test('test ava configuration', t => {
+    t.pass()
+})
+
+// TODO: write tests
